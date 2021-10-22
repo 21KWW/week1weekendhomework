a62e0baa236f979ad5951efe2c51ef2fba0180d4
Keith Wyse <kwyse3436@gmail.com>
2021-10-22 15:49:25 +0100

    Third function completed - adding cash to the till

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -8,3 +8,7 @@ def get_pet_shop_name(pet_shop):
 def get_total_cash(pet_shop):
     return pet_shop["admin"]["total_cash"]
 
+# add or remove cash from the till - does not return a value
+def add_or_remove_cash(pet_shop,cash_value):
+    pet_shop["admin"]["total_cash"] += cash_value
+
