51bbe56a32bc7f9a48e9196980dc7d7a7759593d
Keith Wyse <kwyse3436@gmail.com>
2021-10-22 16:58:19 +0100

    Tests 12 completed - remove a pet by name

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -16,7 +16,7 @@ def add_or_remove_cash(pet_shop,cash_value):
 def get_pets_sold(pet_shop):
     return pet_shop["admin"]["pets_sold"]
 
-# Test 6 - increase the number of pets sold - does not return a value
+# Test 6 - increase the number of pets sold - does not return anything
 def increase_pets_sold(pet_shop, num_pets_sold):
     pet_shop["admin"]["pets_sold"] += num_pets_sold
 
@@ -35,11 +35,24 @@ def get_pets_by_breed(pet_shop, pet_breed):
 
     return pet_name
 
-# Test 10 - find a pet by it's name, return a dictionary of the pet
-# what if there is more than one pet with the same name?
+# Test 10 & 11 - find a pet by it's name, return a dictionary of the pet
+# what if there is more than one pet with the same name? Possibly return a list, but test wouldn't work for that
 def find_pet_by_name(pet_shop, pet_name):
     for each_pet in pet_shop["pets"]:
         if each_pet["name"] == pet_name:
             return each_pet
+
+# Test 12 - remove a pet from the list, returns after the first match
+def remove_pet_by_name(pet_shop, pet_name):
+    each_pet_index=0
+
+    for each_pet in pet_shop["pets"]:
+        if each_pet["name"] == pet_name:
+            pet_shop["pets"].pop(each_pet_index)
+            return
+        
+        each_pet_index += 1
+
+    # print(pet_shop["pets"])
 
 
