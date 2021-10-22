933336be7681ae0bb9001923628a0b18e6371cab
Keith Wyse <kwyse3436@gmail.com>
2021-10-22 15:44:41 +0100

    Second function completed - returning total cash held

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -1,4 +1,10 @@
 # WRITE YOUR FUNCTIONS HERE
+
+# return the name of the pet shop
 def get_pet_shop_name(pet_shop):
     return pet_shop["name"]
+
+# return the total cash held at the moment
+def get_total_cash(pet_shop):
+    return pet_shop["admin"]["total_cash"]
 
